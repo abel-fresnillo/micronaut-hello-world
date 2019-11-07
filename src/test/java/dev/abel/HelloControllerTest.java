cffc99679dbb148dff556b4d12a96b5bd3c992a3
Abel Fresnillo <me@abel.dev>
2019-11-06 21:42:15 -0500

    first commit

---
+++ src/test/java/dev/abel/HelloControllerTest.java
@@ -0,0 +1,28 @@
+package dev.abel;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.RxHttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.test.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@MicronautTest
+public class HelloControllerTest {
+    @Inject
+    @Client("/")
+    RxHttpClient client;
+
+    @Test
+    public void hello_ShouldReturnMessage() {
+        HttpRequest<String> request = HttpRequest.GET("/hello");
+        String body = client.toBlocking().retrieve(request);
+
+        assertNotNull(body);
+        assertEquals("Hello World!", body);
+    }
+}
